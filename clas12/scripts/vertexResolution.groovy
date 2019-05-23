da9dfd32aa6a18b5142168ffeafcf0eab30b5dcc
Tyler <tyler.viduciic@gmail.com>
2019-05-23 17:11:45 -0400

    removed costheta cut
    added momentum cut for vertex

--- clas12/scripts/vertexResolution.groovy
+++ clas12/scripts/vertexResolution.groovy
@@ -1,4 +1,5 @@
 import org.jlab.groot.data.H1F
+import org.jlab.groot.data.TDirectory
 import org.jlab.groot.ui.TCanvas
 import org.jlab.jnp.hipo4.data.Bank
 import org.jlab.jnp.hipo4.data.Event
@@ -14,6 +15,14 @@ String dataFile = "/work/clas12/devita/ctofCalib/rec_004013.hipo";
 H1F hPe_vz = new H1F("hPe_vz", 100, -10, 10);
 hPe_vz.setTitle("Difference between proton and electron z-vertex");
 hPe_vz.setFillColor(45);
+
+HF1 he_vz = new H1F("he_vz", 200, -40, 40);
+hPe_vz.setTitle("electron z-vertex");
+hPe_vz.setFillColor(44);
+
+TDirectory dir = new TDirectory();
+dir.mkdir("/Vertex");
+dir.cd("/Vertex");
 
 
 HipoReader reader = new HipoReader();
@@ -35,12 +44,17 @@ while (reader.hasNext()){
         Particle e = physEvent.getParticle("[11]");
 
         if(p.p()> 1 && e.p() > 3) {
-            hPe_vz.fill(p.vz() - e.vz());
+            hPe_vz.fill(p.vz() - e.vz());\
+            he_vz.fill(e.vz());
         }
     }
 }
 
-TCanvas c1 = new TCanvas("c1", 500, 500);
-c1.draw(hPe_vz);
+//TCanvas c1 = new TCanvas("c1", 500, 500);
+//c1.draw(hPe_vz);
+
+dir.addDataSet(hPe_vz);
+dir.addDataSet(he_vz);
+dir.writeFile("/work/clas12/viducic/rho/clas12/vertexAnalysis.hipo");
 
 println("done");
