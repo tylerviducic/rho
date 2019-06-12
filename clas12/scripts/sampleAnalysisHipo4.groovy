0fe44dfcc000be4a4fb402b51257ac557778a837
Tyler Viducic <tyler.viducic@gmail.com>
2019-06-12 15:22:54 -0400

    testing new run 4013

--- clas12/scripts/sampleAnalysisHipo4.groovy
+++ clas12/scripts/sampleAnalysisHipo4.groovy
@@ -20,7 +20,7 @@ import java.util.Map;
 
 //Same as before, get a list of files in a directory that matches a search pattern.  If you want this Class, ask me :)
 //List<String> dataFiles = FileFinder.getFiles("/w/hallb-scifs17exp/clas12/rg-a/trains/v2/skim4_inclusive/*");
-List<String> dataFiles = FileFinder.getFiles("/w/hallb-scifs17exp/clas12/viducic/data/clas12/testDataFile_filtered_skimmed_3.hipo");
+List<String> dataFiles = FileFinder.getFiles("/w/hallb-scifs17exp/clas12/viducic/data/clas12/testDataFile_filtered_skimmed_4.hipo");
 
 //Step one is declaring histograms.  The histogram class in the JAVA framework is robust with a lot of familiar function
 //Here we see the constructor and setTitle/setFillColor methods but there are many more
