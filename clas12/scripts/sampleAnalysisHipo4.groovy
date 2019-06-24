7c9e5066c7164529649be2a1cdef508dcdd16627
Tyler Viducic <tyler.viducic@gmail.com>
2019-06-24 13:24:07 -0400

    on that rg-k sheit

--- clas12/scripts/sampleAnalysisHipo4.groovy
+++ clas12/scripts/sampleAnalysisHipo4.groovy
@@ -74,7 +74,7 @@ for (String dataFile : dataFiles) {
 
     //Same as before, define bank and event for reader to fill
     Bank particles = new Bank(reader.getSchemaFactory().getSchema("REC::Particle"));
-    Bank conf = new Bank(reader.getSchemaFactory().getSchema("REX::config"));
+    Bank conf = new Bank(reader.getSchemaFactory().getSchema("REC::config"));
     Event event = new Event();
 
     //Filter is probably redundant but it's here anyway.
