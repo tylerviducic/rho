d9c0d73fbc7d8d3fed8d056be9dd8d8170bd0083
Tyler Viducic <tyler.viducic@gmail.com>
2019-06-25 17:25:21 -0400

    excluse for rgk

--- clas12/scripts/sampleAnalysisHipo4.groovy
+++ clas12/scripts/sampleAnalysisHipo4.groovy
@@ -135,7 +135,7 @@ for (String dataFile : dataFiles) {
             //Here is where we do the actual testing. Our first cuts are on the missing momentum and mass2 of pepi+pi-
             //if the missing mass2 of pepi+pi- is < 0.01 and > -0.01, and the missing momentum is > 0.1, the neutral loop
             //executes
-            if (Math.abs(mx_PePipPim.mass2()) < 0.0075 && mx_PePipPim.p() > 0.1) {
+            if (Math.abs(mx_PePipPim.mass2()) < 0.005 && mx_PePipPim.p() > 0.2) {
                 //here i loop over the neutral particles.  I define a particle gam.  I test the angle between this
                 //particle and the missing vector, like i said before
                 for (int i = 0; i < nNeutrals; i++) {
@@ -157,7 +157,7 @@ for (String dataFile : dataFiles) {
                     }
 
                 }
-            if (Math.abs(mx_PePipPim.p() - pgam) < 1.0) {
+            if (Math.abs(mx_PePipPim.p() - pgam) < 0.5) {
                     //For comparison's sake, I fill a histogram with the missing mass of the pe system without any cuts
                     //on cos theta
                     hMxpUncut.fill(mx_P.mass());
