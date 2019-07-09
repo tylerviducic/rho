71404efea60f052819a08f8e7d8d3bbaa85d74f2
Tyler Viducic <tyler.viducic@gmail.com>
2019-07-09 11:08:10 -0400

    fmc script

--- clas12/scripts/rhoFastMC.groovy
+++ clas12/scripts/rhoFastMC.groovy
@@ -1,5 +1,14 @@
 import org.jlab.clas.fastmc.Clas12FastMC
+import org.jlab.jnp.physics.PhysicsEvent
 import org.jlab.jnp.reader.LundReader
 
-Clas12FastMC fmc = new Clas12FastMC(-1, -1);
-LundReader reader = new LundReader()
+String dataFile = "/u/group/clas12/mcdata/generated/lund/ppippim/clasdispr.00.e11.000.emn0.75tmn.09.xs65.61nb.113.0001.dat ";
+
+//Clas12FastMC fmc = new Clas12FastMC(-1, -1);
+LundReader reader = new LundReader(dataFile);
+
+PhysicsEvent event = new PhysicsEvent();
+
+while(reader.nextEvent(event)){
+    println(event.toLundString());
+}
