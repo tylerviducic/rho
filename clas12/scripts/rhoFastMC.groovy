bd4be80565e4d952cd0e8d5b77a91826e3717d6d
Tyler Viducic <tyler.viducic@gmail.com>
2019-07-12 15:21:12 -0400

    cal test

--- clas12/scripts/rhoFastMC.groovy
+++ clas12/scripts/rhoFastMC.groovy
@@ -88,6 +88,7 @@ public class Calorimeter {
         for( int i = 0; i < 6; i ++){
             Shape3D dslice = new Shape3D();
             dslice.addFace(new Triangle3D(slice));
+            dslice.moveTo(0,0,750);
             slice.rotateZ(i * 60);
             newDetector.add(dslice);
         }
