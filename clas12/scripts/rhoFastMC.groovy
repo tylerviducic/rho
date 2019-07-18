c4151838c3c0217770e305c67b001d918b1e6c3b
Tyler Viducic <tyler.viducic@gmail.com>
2019-07-18 15:42:28 -0400

    primitive acceptance test

--- clas12/scripts/rhoFastMC.groovy
+++ clas12/scripts/rhoFastMC.groovy
@@ -7,6 +7,7 @@ import org.jlab.geom.prim.Path3D
 import org.jlab.geom.prim.Point3D
 import org.jlab.geom.prim.Shape3D
 import org.jlab.geom.prim.Triangle3D
+import org.jlab.groot.data.H1F
 import org.jlab.groot.data.H2F
 import org.jlab.groot.ui.TCanvas
 import org.jlab.jnp.physics.Particle
@@ -22,7 +23,10 @@ String dataFile = "/u/group/clas12/mcdata/generated/lund/ppippim/clasdispr.00.e1
 
 H2F hSquare = new H2F("hSquare", "hSquare",100, -500, 500, 100, -500, 500);
 TCanvas c1 = new TCanvas("c1", 600, 600);
+TCanvas c2 = new TCanvas("c2", 600, 600);
 
+H1F hMxp = new H1F("hMxp", "hMxp", 150, 0 , 1.5);
+H1F hMxpWithHits = new H1F("hMxpWithHits", "hMxpWithHits", 150, 0 , 1.5);
 
 //Clas12FastMC fmc = new Clas12FastMC(-1, -1);
 LundReader reader = new LundReader();
@@ -33,23 +37,51 @@ PhysicsEvent event = new PhysicsEvent();
 
 while(reader.nextEvent(event)){
     ParticleList particles = event.getParticleList();
-    for(int i = 0; i < particles.count(); i++){
-        Particle particle = particles.get(i);
-        StraightLine line = new StraightLine(particle);
-        Path3D ppath = line.getPath();
-        boolean intersect = cal.hasIntersection(ppath.getLine(0));
-        println(intersect);
-        ArrayList<Point3D> inters = new ArrayList<Point3D>();
-        int count = cal.intersection(ppath.getLine(0), inters);
-        if(intersect){
-            for(Point3D point : inters){
-                hSquare.fill(point.x(), point.y());
-            }
-        }
+
+    Particle p = event.getParticleByPid(2212);
+    Particle e = event.getParticleByPid(11);
+    Particle pip = event.getParticleByPid(211);
+    Particle pim = event.getParticleByPid(-211)
+
+    Particle mxp = event.getParticle('[b] + [t] - [2212');
+
+//    for(int i = 0; i < particles.count(); i++){
+//
+//        Particle particle = particles.get(i);
+//        StraightLine line = new StraightLine(particle);
+//        Path3D ppath = line.getPath();
+//        boolean intersect = cal.hasIntersection(ppath.getLine(0));
+//        println(intersect);
+//        ArrayList<Point3D> inters = new ArrayList<Point3D>();
+//        int count = cal.intersection(ppath.getLine(0), inters);
+//        if(intersect){
+//            for(Point3D point : inters){
+//                hSquare.fill(point.x(), point.y());
+//            }
+//        }
+//    }
+
+    StraightLine pLine = new StraightLine(p);
+    StraightLine eLine = new StraightLine(e);
+    StraightLine pipLine = new StraightLine(pip);
+    StraightLine pimLine = new StraightLine(pim);
+
+    Path3D pPath = pLine.getPath();
+    Path3D ePath = eLine.getPath();
+    Path3D pipPath = pipLine.getPath();
+    Path3D pimPath = pimLine.getPath();
+
+    hMxp.fill(mxp.mass());
+
+    if(cal.hasIntersection(pPath.getLine(0)) && cal.hasIntersection(ePath.getLine(0)) &&
+            (cal.hasIntersection(pipLine.getPath(0)) || cal.hasIntersection(pimLine.getPath(0)))){
+        hMxpWithHits.fill(mxp.mass());
     }
+
 }
 
-c1.draw(hSquare);
+c1.draw(hMxp);
+c2.draw(hMxpWithHits);
 
 
 public class StraightLine {
