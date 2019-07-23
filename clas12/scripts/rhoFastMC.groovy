c833bc37dfa3955c28ff98bb6884dce52433da2e
Tyler Viducic <tyler.viducic@gmail.com>
2019-07-23 16:34:35 -0400

    DC test

--- clas12/scripts/rhoFastMC.groovy
+++ clas12/scripts/rhoFastMC.groovy
@@ -285,7 +285,7 @@ public class DriftChamberSuperlayer extends Detector {
             Triangle3D sector = createSector();
             sector.show();
             sector.translateXYZ(0,0, this.distanceToTarget);
-            sector.rotateY(Math.toRadians(tilt));
+            sector.rotateY(Math.toRadians(-tilt));
             sector.rotateZ(Math.toRadians(i * 60));
             Shape3D shape = new Shape3D();
             shape.addFace(sector);
