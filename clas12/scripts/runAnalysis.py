0e00b47e75139659c78bccec1f486dd2ab0219af
Tyler Viducic <tyler.viducic@gmail.com>
2019-08-09 14:33:57 -0400

    removed some garbage files, added runCombine

--- clas12/scripts/runAnalysis.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-os.system('/home/gavalian/jaw-0.9/bin/run-groovy RhoAnalysis.groovy')
---
+++ clas12/scripts/runCombine.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+
+run_numbers = []
+
+files = glob.glob("/w/hallb-scifs17exp/clas12/viducic/data/rga/v1/*.hipo")
+
+for f in files:
+    # dst_clas_005165.evio.00580-00584.hipo
+    run_num = f[9:15]
+    if run_num not in run_numbers:
+        run_numbers.append(run_num)
+
+for num in run_numbers:
+    os.mkdir("//w/hallb-scifs17exp/clas12/viducic/data/rga/v1/{}".format(num))
