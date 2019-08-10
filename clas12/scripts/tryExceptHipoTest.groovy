4160d396ca8bab1457be7969b1647b62717b994c
Tyler Viducic <tyler.viducic@gmail.com>
2019-08-10 09:48:38 -0400

    try/catch test

---
+++ clas12/scripts/tryExceptHipoTest.groovy
@@ -0,0 +1,10 @@
+import org.jlab.jnp.hipo4.io.HipoReader
+
+String dataFile = "/lustre/expphy/volatile/clas12/rg-a/production/recon/pass1/dst/v2/005163/dst_clas_005163.evio.00315-00319.hipo";
+
+HipoReader reader = new HipoReader();
+try {
+    reader.open(dataFile);
+} catch (FileNotFoundException e){
+    println("Exception caught.");
+}
