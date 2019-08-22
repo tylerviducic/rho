2ab455beaf61a0b51b1839f7c73fc6253e40eaee
Tyler <tyler.viduciic@gmail.com>
2019-08-22 14:43:50 -0400

    first attempt for omega

---
+++ clas12/scripts/src/rgaOmegaAnalysis.groovy
@@ -0,0 +1,147 @@
+import org.jlab.groot.data.H1F
+import org.jlab.groot.data.TDirectory
+import org.jlab.jnp.hipo4.data.Bank
+import org.jlab.jnp.hipo4.data.Event
+import org.jlab.jnp.hipo4.io.HipoReader
+import org.jlab.jnp.physics.EventFilter
+import org.jlab.jnp.physics.Particle
+import org.jlab.jnp.physics.PhysicsEvent
+import org.jlab.jnp.reader.DataManager
+import org.jlab.jnp.utils.file.FileUtils
+
+List<String> dataFiles = FileFinder.getFiles("/w/hallb-scifs17exp/clas12/rg-a/trains/v16_v2/skim8_ep/*");
+
+H1F hMxp = new H1F("hMxp", 200, 0, 2);
+H1F hIMPipPimPi0 = new H1F("hIMPipPimPi0", 200, 0, 2);
+
+TDirectory dir = new TDirectory();
+dir.mkdir("/Cuts");
+dir.mkdir("/Plots");
+
+EventFilter filter = new EventFilter("11:2212:211:-211:22:22");
+
+double beamEnergy = 10.6;
+
+for(String dataFile : dataFiles){
+    HipoReader reader = new HipoReader();
+    reader.open(dataFile);
+
+    Bank particles = new Bank(reader.getSchemaFactory().getSchema("REC::Particle"));
+    Bank conf = new Bank(reader.getSchemaFactory().getSchema("RUN::config"));
+    Event event = new Event();
+
+    while(reader.hasNext()){
+        reader.nextEvent(event);
+        event.read(particles);
+        event.read(conf);
+
+        PhysicsEvent physEvent = DataManager.getPhysicsEvent(beamEnergy, particles);
+
+        if(filter.isValid(physEvent)){
+            Particle mxp = physEvent.getParticle("[b] + [t] - [2212] - [11]");
+            Particle imPipPimPi0 = physEvent.getParticle("[211] + [-211] + [22,0] + [22, 1]");
+
+            hMxp.fill(mxp.mass());
+            hIMPipPimPi0.fill(imPipPimPi0);
+        }
+    }
+    reader.close();
+}
+
+dir.cd("/Plots");
+dir.addDataSet(hMxp);
+dir.addDataSet(hIMPipPimPi0);
+
+dir.writeFile("/work/clas12/viducic/rho/clas12/results/exclusiveOmegaAnalysis_RGA.hipo");
+println("done");
+
+
+public class FileFinder {
+
+    public FileFinder() {
+    }
+
+    private static List<String> listOfFiles = new ArrayList<String>();
+    private static String newKeyWord = "";
+    public static int DEBUG_MODE = 0;
+
+    public static List<String> getFiles(List<String> listOfDirs, String fileName) {
+        for (String dir : listOfDirs) {
+            getFiles(dir, fileName);
+        }
+        return listOfFiles;
+    }
+
+    public static List<String> getFilesFromSubdirs(String directory, String wildcard) {
+        List<String> listOfDirs = getSubdirs(directory);
+        return getFiles(listOfDirs, wildcard);
+    }
+
+
+    public static List<String> getFiles(String directory, String wildcard) {
+        String newDir = "";
+        if (!directory.endsWith("/")) {
+            newDir = directory + "/";
+        } else {
+            newDir = directory;
+        }
+        List<String> filesInDirectory = FileUtils.getFileListInDir(directory);
+
+        if (wildcard.contains("*")) {
+            newKeyWord = newDir + wildcard.replace("*", ".*");
+        } else {
+            newKeyWord = newDir + wildcard;
+        }
+        for (String f : filesInDirectory) {
+            if (f.matches(newKeyWord)) {
+                listOfFiles.add(f.toString());
+            }
+        }
+        return this.listOfFiles;
+    }
+
+    public static List<String> getDirectoryName(String fullPath) {
+        List<String> dirFile = new ArrayList<String>();
+        int start = fullPath.lastIndexOf("/");
+        dirFile.add(0, fullPath.substring(0, start + 1));
+        dirFile.add(1, fullPath.substring(start + 1));
+        return dirFile;
+    }
+
+    public static List<String> getFiles(String fullPath) {
+        List<String> dirCombo = getDirectoryName(fullPath);
+        return getFiles(dirCombo.get(0), dirCombo.get(1));
+    }
+
+    public static List<String> getSubdirs(String directory) {
+        if (DEBUG_MODE > 0) {
+            System.out.println(">>> scanning directory : " + directory);
+        }
+
+        List<String> dirList = new ArrayList();
+        File[] dirs = (new File(directory)).listFiles();
+        if (dirs == null) {
+            if (DEBUG_MODE > 0) {
+                System.out.println(">>> scanning directory : directory does not exist");
+            }
+
+            return dirList;
+        } else {
+            File[] var3 = dirs;
+            int var4 = dirs.length;
+
+            for (int var5 = 0; var5 < var4; ++var5) {
+                File dir = var3[var5];
+                if (dir.isDirectory()) {
+                    if (!dir.getName().startsWith(".") && !dir.getName().endsWith("~")) {
+                        dirList.add(dir.getAbsolutePath());
+                    } else if (DEBUG_MODE > 0) {
+                        System.out.println("[FileUtils] ----> skipping file : " + dir.getName());
+                    }
+                }
+            }
+
+            return dirList;
+        }
+    }
+}
