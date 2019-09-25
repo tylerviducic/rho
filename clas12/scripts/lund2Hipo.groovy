1afc973f48445fbbb8e751908e54ba9cc78bc2d9
Tyler Viducic <tyler.viducic@gmail.com>
2019-09-25 13:44:56 -0400

    fastMC for .hipo

--- clas12/scripts/lund2Hipo.groovy
+++ clas12/scripts/lund2Hipo.groovy
@@ -7,7 +7,7 @@ import org.jlab.jnp.reader.LundReader
 import org.jlab.jnp.utils.benchmark.ProgressPrintout
 import org.jlab.jnp.utils.file.FileUtils
 
-String directory = "/scratch/viducic/tmp0";
+String directory = "/scratch/viducic/tmp1";
 List<String> fileList = FileUtils.getFileListInDir(directory);
 
 Schema.SchemaBuilder schemaBuilder = new Schema.SchemaBuilder("mc::event", 22001, 1);
@@ -30,7 +30,7 @@ writer.getSchemaFactory().addSchema(schema);
 writer.setCompressionType(2);
 writer.setMaxSize(16777216).setMaxEvents(1000000);
 writer.setCompressionType(2);
-writer.open("/work/clas12/viducic/data/clas12/rho_mc_0.hipo");
+writer.open("/work/clas12/viducic/data/clas12/rho_mc_1.hipo");
 int counter = 0;
 int eventCounter = 0;
 Iterator var10 = fileList.iterator();
