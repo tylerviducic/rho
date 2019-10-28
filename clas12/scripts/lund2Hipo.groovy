97a3f855a972b4af5bb78b9fba77ca2901b3e4f0
Tyler Viducic <tyler.viducic@gmail.com>
2019-10-28 18:22:16 -0400

    lund2hipo new usage

--- clas12/scripts/lund2Hipo.groovy
+++ clas12/scripts/lund2Hipo.groovy
@@ -8,8 +8,10 @@ import org.jlab.jnp.reader.LundReader
 import org.jlab.jnp.utils.benchmark.ProgressPrintout
 import org.jlab.jnp.utils.file.FileUtils
 
-String directory = " /w/hallb-scifs17exp/clas12/avakian/mc/mcaugust2019/T-1.00_S-1.0/clasdis/cooked631";
+String directory = " /w/hallb-scifs17exp/clas12/avakian/mc/mcaugust2019/T-1.00_S-1.0/clasdis/cooked631/";
 List<String> fileList = FileUtils.getFileListInDir(directory);
+
+println(fileList.size());
 
 EventFilter filter = new EventFilter("11:2212:211:-211:22");
 
