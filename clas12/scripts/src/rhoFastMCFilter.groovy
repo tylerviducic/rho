c5826ddfeb79c87e91c36fb58a3e2126cc6a91fb
Tyler Viducic <tyler.viducic@gmail.com>
2019-10-28 18:43:29 -0400

    wrong file format. added new script

---
+++ clas12/scripts/src/rhoFastMCFilter.groovy
@@ -0,0 +1,130 @@
+import org.jlab.jnp.hipo4.data.Bank
+import org.jlab.jnp.hipo4.data.Event
+import org.jlab.jnp.hipo4.io.HipoReader
+import org.jlab.jnp.hipo4.io.HipoWriter
+import org.jlab.jnp.physics.EventFilter
+import org.jlab.jnp.physics.PhysicsEvent
+import org.jlab.jnp.reader.DataManager
+import org.jlab.jnp.utils.file.FileUtils
+
+List<String> dataFiles = FileFinder.getFilesFromSubdirs("/w/hallb-scifs17exp/clas12/avakian/mc/mcaugust2019/T-1.00_S-1.0/clasdis/cooked631/", "*.hipo");
+EventFilter filter = new EventFilter("11:2212:211:-211:22");
+HipoReader firstReader = new HipoReader();
+firstReader.open(dataFiles[0]);
+HipoWriter writer = new HipoWriter(firstReader.getSchemaFactory());
+firstReader.close();
+
+int eventCounter = 0;
+
+writer.open("/work/clas12/viducic/data/clas12/rho_mc_higherQ2.hipo");
+
+for(String dataFile: dataFiles){
+    HipoReader reader = new HipoReader();
+    reader.open(dataFile);
+
+    Bank particles = new Bank(reader.getSchemaFactory().getSchema("MC::Event"));
+    Event event = new Event();
+
+    reader.nextEvent(event);
+    event.read(particles);
+
+    PhysicsEvent physicsEvent = DataManager.getPhysicsEvent(10.6, particles);
+    if(filter.isValid(physicsEvent)){
+        eventCounter++;
+        writer.addEvent(event);
+    }
+}
+
+println(eventCounter);
+
+
+public class FileFinder {
+
+    public FileFinder() {
+    }
+
+    private static List<String> listOfFiles = new ArrayList<String>();
+    private static String newKeyWord = "";
+    public static int DEBUG_MODE = 0;
+
+    public static List<String> getFiles(List<String> listOfDirs, String fileName) {
+        for (String dir : listOfDirs) {
+            getFiles(dir, fileName);
+        }
+        return listOfFiles;
+    }
+
+    public static List<String> getFilesFromSubdirs(String directory, String wildcard) {
+        List<String> listOfDirs = getSubdirs(directory);
+        return getFiles(listOfDirs, wildcard);
+    }
+
+
+    public static List<String> getFiles(String directory, String wildcard) {
+        String newDir = "";
+        if (!directory.endsWith("/")) {
+            newDir = directory + "/";
+        } else {
+            newDir = directory;
+        }
+        List<String> filesInDirectory = FileUtils.getFileListInDir(directory);
+
+        if (wildcard.contains("*")) {
+            newKeyWord = newDir + wildcard.replace("*", ".*");
+        } else {
+            newKeyWord = newDir + wildcard;
+        }
+        for (String f : filesInDirectory) {
+            if (f.matches(newKeyWord)) {
+                listOfFiles.add(f.toString());
+            }
+        }
+        return this.listOfFiles;
+    }
+
+    public static List<String> getDirectoryName(String fullPath) {
+        List<String> dirFile = new ArrayList<String>();
+        int start = fullPath.lastIndexOf("/");
+        dirFile.add(0, fullPath.substring(0, start + 1));
+        dirFile.add(1, fullPath.substring(start + 1));
+        return dirFile;
+    }
+
+    public static List<String> getFiles(String fullPath) {
+        List<String> dirCombo = getDirectoryName(fullPath);
+        return getFiles(dirCombo.get(0), dirCombo.get(1));
+    }
+
+    public static List<String> getSubdirs(String directory) {
+        if (DEBUG_MODE > 0) {
+            System.out.println(">>> scanning directory : " + directory);
+        }
+
+        List<String> dirList = new ArrayList();
+        File[] dirs = (new File(directory)).listFiles();
+        if (dirs == null) {
+            if (DEBUG_MODE > 0) {
+                System.out.println(">>> scanning directory : directory does not exist");
+            }
+
+            return dirList;
+        } else {
+            File[] var3 = dirs;
+            int var4 = dirs.length;
+
+            for (int var5 = 0; var5 < var4; ++var5) {
+                File dir = var3[var5];
+                if (dir.isDirectory()) {
+                    if (!dir.getName().startsWith(".") && !dir.getName().endsWith("~")) {
+                        dirList.add(dir.getAbsolutePath());
+                    } else if (DEBUG_MODE > 0) {
+                        System.out.println("[FileUtils] ----> skipping file : " + dir.getName());
+                    }
+                }
+            }
+
+            return dirList;
+        }
+    }
+}
+
