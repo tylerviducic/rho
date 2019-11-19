81e52a49b4f22d1b1c446203b3cdb2fba6cf9a14
Tyler Viducic <tyler.viducic@gmail.com>
2019-11-19 14:16:55 -0500

    primakoff work

--- clas12/scripts/premakoffPiPi.groovy
+++ clas12/scripts/premakoffPiPi.groovy
@@ -16,7 +16,8 @@ reader.addDir(directory);
 reader.open();
 
 int eventCounter = 0;
-int channelCounter = 0;
+int filterCounter = 0;
+int noFilterCounter = 0;
 
 Event event = new Event();
 Bank particle = new Bank(reader.getSchemaFactory().getSchema("REC::Particle"));
@@ -28,23 +29,28 @@ while (reader.hasNext()){
     event.read(particle);
     eventCounter++;
     if(eventCounter % 10000 == 0){
-        System.out.println("Psyche counter = " + eventCounter);
+        System.out.println("Event counter = " + eventCounter);
     }
 
     PhysicsEvent physicsEvent = DataManager.getPhysicsEvent(10.6, particle);
 
-    //if(physicsEvent.getParticleList().count() > 0 &&  physicsEvent.getParticle(0).pid() == -211 ){
+    if(physicsEvent.getParticleList().count() > 0 &&  physicsEvent.getParticle(0).pid() == -211
+            && physicsEvent.countByPid(2212) == 1  && physicsEvent.countByPid(211) == 1
+            && physicsEvent.countByCharge(1) == 2 && physicsEvent.countByCharge(-1) == 1){
+        noFilterCounter++;
+    }
         ParticleList particleList = physicsEvent.getParticleList();
         if(filter.isValid(physicsEvent) && physicsEvent.getParticleByPid(11, 0).theta() < Math.toRadians(5)) {
-            channelCounter++;
+            filterCounter++;
         }
-    //}
+
 }
 
 System.out.println("++++++++++++++++++++++++++++++++++++");
 System.out.println("++++++++++++++++++++++++++++++++++++\n");
-System.out.println("Number of p pi+ pi- events with no electron: " + channelCounter);
+System.out.println("Number of p pi+ pi- events with electron: " + filterCounter);
 System.out.println("Total number of events = " + eventCounter);
-System.out.println("Percentage of events with wanted final state: " + ((double)(channelCounter)/(double)(eventCounter) * 100));
-
+System.out.println("Percentage of events with wanted final state (no e): " + ((double)(filterCounter)/(double)(eventCounter) * 100));
+System.out.println("Number of p pi+ pi- events with no electron: " + noFilterCounter);
+System.out.println("Percentage of events with wanted final state : " + ((double)(noFilterCounter)/(double)(eventCounter) * 100));
 
