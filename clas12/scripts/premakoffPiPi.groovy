c174641a709d4dbc2497c0454b1cf4ce335f6deb
Tyler Viducic <tyler.viducic@gmail.com>
2019-11-21 09:13:04 -0500

    electron detected analysis

--- clas12/scripts/premakoffPiPi.groovy
+++ clas12/scripts/premakoffPiPi.groovy
@@ -76,9 +76,10 @@ while (reader.hasNext()){
         hEDPxPyPt.fill(pxPt, pyPt);
         hEDq2.fill(q2);
 
-        //if(q2 < )
-        hEDImPipPimTheta.fill(imPipPim.mass(), Math.toDegrees(imPipPim.theta()));
-
+        if(q2 < 0.02 && Math.abs(pyPt) < 0.15 && Math.ans(pxPt) < 0.15
+                && Math.abs(missingEPPipPim.mass2()) < 0.01 ) {
+            hEDImPipPimTheta.fill(imPipPim.mass(), Math.toDegrees(imPipPim.theta()));
+        }
 
     }
 }
