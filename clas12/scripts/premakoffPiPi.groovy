dce02d4a35141b1abb3238a5769936f0e6963b00
Tyler Viducic <tyler.viducic@gmail.com>
2019-11-22 15:05:50 -0500

    added undetected loop

--- clas12/scripts/premakoffPiPi.groovy
+++ clas12/scripts/premakoffPiPi.groovy
@@ -10,6 +10,8 @@ import org.jlab.jnp.hipo4.io.HipoChain
 import org.jlab.groot.data.H2F
 import org.jlab.groot.ui.TCanvas
 
+///////////////////////       e detected histos        ///////////////////////
+
 H1F hEDPyPt = new H1F("hEDPyPt", 100, -0.5, 0.5);
 hEDPyPt.setTitle("missing pty/missing p");
 H1F hEDPxPt = new H1F("hEDPxPt", 100, -0.5, 0.5);
@@ -22,9 +24,28 @@ H1F hEDq2 = new H1F("hEDq2", 50, 0, 0.1);
 hEDq2.setTitle("Q2");
 H2F hEDImPipPimTheta = new H2F("hImPipPimTheta", 60, 0.5, 1.1, 50, 0, 50);
 hEDImPipPimTheta.setTitle("IMpi+pi- vs theta of p(pi+pi-)");
+H1F hDiffPT = new H1F("hDiffPT", 100, -0.5, 0.5);
+hDiffPT.setTitle("Difference between Missing PT and ePT");
+
+///////////////////////      no e detected histos       ///////////////////////
+
+H1F hPyPt = new H1F("hEDPyPt", 100, -0.5, 0.5);
+hPyPt.setTitle("missing pty/missing p");
+H1F hPxPt = new H1F("hEDPxPt", 100, -0.5, 0.5);
+hPxPt.setTitle("missing ptx/missing p");
+H2F hPxPyPt = new H2F("hEDPxPyPt", 100, -0.2, 0.2, 100, -0.2, 0.2);
+hPxPyPt.setTitle("ptx/p vs pty/p");
+H1F hMm2PPipPim = new H1F("hEDMmPPipPim", 100, -0.05, 0.05);
+hMm2PPipPim.setTitle("Missing mass2 of e'p'pi+pi-");
+H1F hq2 = new H1F("hEDq2", 50, 0, 0.1);
+hq2.setTitle("Q2");
+H2F hImPipPimTheta = new H2F("hImPipPimTheta", 60, 0.5, 1.1, 50, 0, 50);
+hImPipPimTheta.setTitle("IMpi+pi- vs theta of p(pi+pi-)");
+
 
 TDirectory dir = new TDirectory();
 dir.mkdir("/ElectronDetected");
+dir.mkdir("NoElectronDetected");
 
 //String directory = "/w/hallb-scifs17exp/clas12/rg-a/trains/pass1/v1_4/skim04_inclusive";
 String subDirectory = "/lustre19/expphy/cache/clas12/rg-a/production/reconstructed/Fall2018/Torus-1/pass1/v1/005";
@@ -69,17 +90,36 @@ while (reader.hasNext()){
 
     PhysicsEvent physicsEvent = DataManager.getPhysicsEvent(10.6, particle);
 
-    //No Electron detected loop
+////////////////////    No Electron detected loop    /////////////////////////
     if(physicsEvent.getParticleList().count() > 0 &&  physicsEvent.getParticle(0).pid() == -211
-            && physicsEvent.countByPid(2212) == 1  && physicsEvent.countByPid(211) == 1
-            && physicsEvent.countByCharge(1) == 2 && physicsEvent.countByCharge(-1) == 1){
+        && physicsEvent.countByPid(2212) == 1  && physicsEvent.countByPid(211) == 1
+        && physicsEvent.countByCharge(1) == 2 && physicsEvent.countByCharge(-1) == 1){
 
         noFilterCounter++;
 
+        Particle missingPPipPim = physicsEvent.getParticle("[b] + [t] - [2212] - [211] - [-211]");
+        Particle imPipPim = physicsEvent.getParticle("[211] + [-211]");
+
+        double q2 = getQ2(physicsEvent.beamParticle(), missingPPipPim);
+        double pyPt = missingPPipPim.py()/missingPPipPim.p();
+        double pxPt = missingPPipPim.px()/missingPPipPim.p();
+
+        hMm2PPipPim.fill(missingPPipPim.mass2());
+        if (Math.abs(missingPPipPim.mass2()) < 0.02){
+            hPxPt.fill(pxPt);
+            hPyPt.fill(pyPt);
+            hPxPyPt.fill(pxPt, pyPt);
+            hq2.fill(q2);
+        }
+
+        if(q2 < 0.02 && Math.abs(pyPt) < 0.2 && Math.abs(pxPt) < 0.2
+            && Math.abs(missingPPipPim.mass2()) < 0.02 ) {
+            hImPipPimTheta.fill(imPipPim.mass(), Math.toDegrees(imPipPim.theta()));
+        }
     }
 
-    //Electron detected loop
-    if(filter.isValid(physicsEvent) && physicsEvent.getParticleByPid(11, 0).theta() < Math.toRadians(5)) {
+////////////////////       Electron detected loop      /////////////////////////
+    else if(filter.isValid(physicsEvent) && physicsEvent.getParticleByPid(11, 0).theta() < Math.toRadians(5)) {
 
         filterCounter++;
 
@@ -91,7 +131,8 @@ while (reader.hasNext()){
         double q2 = getQ2(physicsEvent.beamParticle(), electron);
         double pyPt = missingPPipPim.py()/missingPPipPim.p();
         double pxPt = missingPPipPim.px()/missingPPipPim.p();
-        
+        double missingPT = Math.sqrt(missingPPipPim.px()*missingPPipPim.px() + missingPPipPim.py()*missingPPipPim.py());
+        double ePT = Math.sqrt(electron.px() * electron.px() + electron.py()*electron.py());
 
         hEDMm2EPPipPim.fill(missingEPPipPim.mass2());
         if(Math.abs(missingEPPipPim.mass2()) < 0.02) {
@@ -99,46 +140,45 @@ while (reader.hasNext()){
             hEDPyPt.fill(pyPt);
             hEDPxPyPt.fill(pxPt, pyPt);
             hEDq2.fill(q2);
+            hDiffPT.fill(missingPT-ePT);
         }
 
         if(q2 < 0.02 && Math.abs(pyPt) < 0.2 && Math.abs(pxPt) < 0.2
-                && Math.abs(missingEPPipPim.mass2()) < 0.02 ) {
+            && Math.abs(missingEPPipPim.mass2()) < 0.02 ) {
             hEDImPipPimTheta.fill(imPipPim.mass(), Math.toDegrees(imPipPim.theta()));
         }
-
     }
 }
 
-
-
 dir.cd("/ElectronDetected");
 dir.addDataSet(hEDPxPyPt, hEDPxPt, hEDPyPt, hEDq2, hEDImPipPimTheta, hEDMm2EPPipPim);
+dir.cd("/NoElectronDetected");
+dir.addDataSet(hPxPyPt, hPxPt, hPyPt, hq2, hImPipPimTheta, hMm2EPPipPim);
 dir.writeFile("/w/hallb-scifs17exp/clas12/viducic/premakoff/results/premakoffResults.hipo");
 
-TCanvas c1 = new TCanvas("c1", 1000, 1000);
-c1.divide(2, 3);
-c1.cd(0);
-c1.draw(hEDImPipPimTheta);
-c1.cd(1);
-c1.draw(hEDMm2EPPipPim);
-c1.cd(2);
-c1.draw(hEDPxPt);
-c1.cd(3);
-c1.draw(hEDPyPt);
-c1.cd(4);
-c1.draw(hEDPxPyPt);
-c1.cd(5);
-c1.draw(hEDq2);
+//TCanvas c1 = new TCanvas("c1", 1000, 1000);
+//c1.divide(2, 3);
+//c1.cd(0);
+//c1.draw(hEDImPipPimTheta);
+//c1.cd(1);
+//c1.draw(hEDMm2EPPipPim);
+//c1.cd(2);
+//c1.draw(hEDPxPt);
+//c1.cd(3);
+//c1.draw(hEDPyPt);
+//c1.cd(4);
+//c1.draw(hEDPxPyPt);
+//c1.cd(5);
+//c1.draw(hEDq2);
 
 System.out.println("++++++++++++++++++++++++++++++++++++");
 System.out.println("++++++++++++++++++++++++++++++++++++\n");
+
 System.out.println("Number of p pi+ pi- events with electron: " + filterCounter);
 System.out.println("Total number of events = " + eventCounter);
-System.out.println("Percentage of events with wanted final state (no e): " + ((double)(filterCounter)/(double)(eventCounter) * 100));
 System.out.println("Number of p pi+ pi- events with no electron: " + noFilterCounter);
-System.out.println("Percentage of events with wanted final state : " + ((double)(noFilterCounter)/(double)(eventCounter) * 100));
 System.out.println("Number of events with final state + cuts: " + hEDImPipPimTheta.integral());
-/////////////          Methods              ////////////////
+/////////////           Methods              ////////////////
 
 public static double getQ2(Particle particle1, Particle particle2){
     return 4 * particle1.e() * particle2.e() * Math.sin(particle2.theta() /2) * Math.sin(particle2.theta()/2);
