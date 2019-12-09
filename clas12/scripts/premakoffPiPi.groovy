437520c335e67e97c5d3a1447159285430c33c88
Tyler Viducic <tyler.viducic@gmail.com>
2019-12-09 16:41:14 -0500

    proton can be in central now

--- clas12/scripts/premakoffPiPi.groovy
+++ clas12/scripts/premakoffPiPi.groovy
@@ -213,8 +213,13 @@ public static int getBinIndex(Particle particle){
 public static boolean inForward(PhysicsEvent physicsEvent){
     ParticleList particleList = physicsEvent.getParticleList();
     for(int i = 0; i < particleList.count(); i++){
-        if (Math.toDegrees(particleList.get(i).theta()) > 35 &&particleList.get(i).pid()!=2212){
-            return false;
+        if (Math.toDegrees(particleList.get(i).theta()) > 35 ){
+            if (particleList.get(i).pid() == 2212){
+                continue;
+            }
+            else {
+                return false
+            };
         }
     }
     return true;
