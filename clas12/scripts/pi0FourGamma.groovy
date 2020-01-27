17169582674a85d034d4b09430a857abe8383e43
Tyler Viducic <tyler.viducic@gmail.com>
2020-01-27 13:21:45 -0500

    added 4 photon script

--- clas12/scripts/pi0FourGamma.groovy
+++ clas12/scripts/pi0FourGamma.groovy
@@ -9,6 +9,8 @@ import org.jlab.jnp.reader.DataManager
 
 H1F hMM2pe = new H1F("hMM2pe", 230, -0.3, 2.0);
 hMM2pe.setTitle("Missing Mass^2 of detected proton and electron");
+H1F hMM2all = new H1F("hMM2all", 100, -0.5, 0.5);
+hMM2all.setTitle("Missing mass 2 of p, e, 4 photons");
 
 String file = "/w/hallb-scifs17exp/clas12/viducic/data/clas12/premakoff/photons.hipo";
 HipoChain reader = new HipoChain();
@@ -25,11 +27,15 @@ while (reader.hasNext()){
     PhysicsEvent physicsEvent = DataManager.getPhysicsEvent(10.6, particle);
 
     Particle missingPe = physicsEvent.getParticle("[b] + [t] - [2212] - [11]");
+    Particle missingPeGamGamGamGam = physicsEvent.getParticle("[b] + [t] - [2212] - [11] - [22,0] - [22, 1] - [22,3] - [22,3]")
 
     hMM2pe.fill(missingPe.mass2());
+    hMM2all.fill(missingPeGamGamGamGam.mass2());
 }
 
 TCanvas c1 = new TCanvas("c1", 500, 500);
 c1.draw(hMM2pe);
+TCanvas c2 = new TCanvas("c2", 500, 500);
+c2.draw(hMM2all);
 
 
