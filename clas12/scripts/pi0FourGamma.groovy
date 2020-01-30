a90bf8338cbe943cb99413cbf04a78f532b567e7
Tyler <tyler.viduciic@gmail.com>
2020-01-30 10:41:24 -0500

    hey

--- clas12/scripts/pi0FourGamma.groovy
+++ clas12/scripts/pi0FourGamma.groovy
@@ -54,7 +54,7 @@ while (reader.hasNext()){
             hPion2.fill(pion2.mass());
             hPion1Pion2.fill(pion1.mass(), pion2.mass());
 //            if (pion2.mass() > 0.1 && pion2.mass() < 0.16 && gam4.mass() > 0.8 && gam4.mass() < 1.1){
-        if ( gam4.mass() > 0.8 && gam4.mass() < 1.1){
+        if ( gam4.mass() > 0.4 && gam4.mass() < 0.65){
 
             hIM4gam.fill(gam4.mass());
                 hMM2pe.fill(missingPe.mass());}
