2c0da744a314bb138dfc96ecdef5c33c84339cbd
Tyler Viducic <tyler.viducic@gmail.com>
2020-02-03 14:18:39 -0500

    lambda work

--- clas12/scripts/lambda.groovy
+++ clas12/scripts/lambda.groovy
@@ -11,10 +11,14 @@ import org.jlab.jnp.reader.DataManager
 H1F hMMProtonElectron = new H1F("hMMProtonElectron", 200, 0, 1);
 H1F hIMProtonPion = new H1F("hIMProtonPion", 250, 1.0, 2.5);
 H1F hMMElectronKaon = new H1F("hIMElectronKaon", 100, 1.0, 2.5);
+H1F hMMProtonElectronCut = new H1F("hMMProtonElectronCut", 200, 0, 1);
+H1F hIMProtonPionCut = new H1F("hIMProtonPionCut", 250, 1.0, 2.5)
 
 TCanvas c1 = new TCanvas("c1", 500, 500);
 TCanvas c2 = new TCanvas("c2", 500, 500);
 TCanvas c3 = new TCanvas("c3", 500, 500);
+TCanvas c4 = new TCanvas("c4", 500, 500);
+TCanvas c5 = new TCanvas("c5", 500, 500);
 
 String directory = "/w/hallb-scifs17exp/clas12/viducic/data/clas12/lambda/tagger";
 //String directory = "/w/hallb-scifs17exp/clas12/viducic/data/clas12/lambda/forward";
@@ -39,20 +43,23 @@ while (reader.hasNext()){
 
     hIMProtonPion.fill(protonPion.mass());
     if(protonPion.mass() > 0.95 && protonPion.mass() < 1.2){
-        hMMProtonElectron.fill(missingKaon.mass());
+        hMMProtonElectronCut.fill(missingKaon.mass());
     }
 
-//    hMMProtonElectron.fill(missingKaon.mass());
-//    if(missingKaon.mass() > 0.4 && missingKaon.mass() < 0.6){
-//        hIMProtonPion.fill(protonPion.mass());
+    hMMProtonElectron.fill(missingKaon.mass());
+    if(missingKaon.mass() > 0.4 && missingKaon.mass() < 0.6){
+        hIMProtonPionCut.fill(protonPion.mass());
 //        if (filter.isValid(physicsEvent)){
 //            Particle electronKaon = physicsEvent.getParticle("[b] + [t] - [11] - [321]");
 //            hMMElectronKaon.fill(electronKaon.mass());
 //        }
-//    }
+    }
 
 }
 
-c1.draw(hMMProtonElectron);
+c1.draw(hMMProtonElectronCut);
 c2.draw(hIMProtonPion);
 c3.draw(hMMElectronKaon);
+
+c4.draw(hMMProtonElectron);
+c5.draw(hMMProtonElectron);
