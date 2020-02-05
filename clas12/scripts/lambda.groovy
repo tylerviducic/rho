c24536437f7c78c7d2f36858763677b56c6b790d
Tyler Viducic <tyler.viducic@gmail.com>
2020-02-05 15:57:26 -0500

    lambda work

--- clas12/scripts/lambda.groovy
+++ clas12/scripts/lambda.groovy
@@ -1,4 +1,5 @@
 import org.jlab.groot.data.H1F
+import org.jlab.groot.data.H1FC
 import org.jlab.groot.ui.TCanvas
 import org.jlab.jnp.hipo4.data.Bank
 import org.jlab.jnp.hipo4.io.HipoChain
@@ -19,7 +20,16 @@ hMMProtonElectronCut.setTitle("Missing mass of Proton-Electron-Pion with cuts");
 H1F hIMProtonPionCut = new H1F("hIMProtonPionCut", 250, 1.0, 1.25)
 hIMProtonPionCut.setTitle("Invariant mass of Proton-Pion  with cuts");
 
+H1F hProtonVertex = new H1F("hProtonVertex", 100, -50, 50);
+H1F hElectronVertex = new H1F("hElectronVertex", 100, -50, 50);
+H1F hPionVertex = new H1F("hPioVertex", 100, -50, 50);
+
+//H1FC hK = new H1FC("hK", 100, 0.2, 1.2);
+//hK.addCut(0.45, 0.65);
+//hk.
+
 TCanvas c1 = new TCanvas("c1", 1000, 1000);
+TCanvas c2 = new TCanvas("c2", 1000, 1000);
 
 c1.divide(2,2);
 c1.getCanvas().initTimer(1000);
@@ -28,7 +38,11 @@ c1.cd(1).draw(hMMProtonElectronCut);
 c1.cd(2).draw(hIMProtonPionCut);
 c1.cd(3).draw(hMMProtonElectron);
 
-
+c2.divide(1, 3);
+c2.getCanvas().initTimer(1000);
+c2.cd(0).draw(hProtonVertex);
+c2.cd(1).draw(hElectronVertex);
+c2.cd(2).draw(hPionVertex);
 
 /*TCanvas c2 = new TCanvas("c2", 500, 500);
 TCanvas c3 = new TCanvas("c3", 500, 500);
@@ -47,6 +61,8 @@ Bank particle = new Bank(reader.getSchemaFactory().getSchema("REC::Particle"));
 
 EventFilter filter = new EventFilter("11:2212:-211:Xn:X+:X-");
 
+//10-2
+
 while (reader.hasNext()){
     reader.nextEvent(event);
     event.read(particle);
@@ -55,6 +71,10 @@ while (reader.hasNext()){
 
     Particle missingKaon = physicsEvent.getParticle("[b] + [t] - [2212] - [11] - [-211]");
     Particle protonPion = physicsEvent.getParticle("[2212] + [-211]");
+
+    hProtonVertex.fill(physicsEvent.getParticle("2212").vz());
+    hElectronVertex.fill(physicsEvent.getParticle("11").vz());
+    hPionVertex.fill(physicsEvent.getParticle("-211").vz());
 
     hIMProtonPion.fill(protonPion.mass());
     if(protonPion.mass() > 1.1 && protonPion.mass() < 1.125){
