0c9f12440c13fbe5f868c1f129871c979f8df649
Tyler Viducic <tyler.viducic@gmail.com>
2020-02-04 19:28:02 -0500

    lambda work

--- clas12/scripts/lambda.groovy
+++ clas12/scripts/lambda.groovy
@@ -10,21 +10,31 @@ import org.jlab.jnp.reader.DataManager
 
 H1F hMMProtonElectron = new H1F("hMMProtonElectron", 200, 0, 1);
 hMMProtonElectron.setTitle("Missing mass of Proton-Electron-Pion with no cuts");
-H1F hIMProtonPion = new H1F("hIMProtonPion", 250, 1.0, 2.5);
+H1F hIMProtonPion = new H1F("hIMProtonPion", 250, 1.0, 1.25);
 hIMProtonPion.setTitle("Invariant mass of Proton-Pion  with no cuts");
 H1F hMMElectronKaon = new H1F("hIMElectronKaon", 100, 1.0, 2.5);
 hMMElectronKaon.setTitle("Missing mass of electron-kaon");
 H1F hMMProtonElectronCut = new H1F("hMMProtonElectronCut", 200, 0, 1);
 hMMProtonElectronCut.setTitle("Missing mass of Proton-Electron-Pion with cuts");
-H1F hIMProtonPionCut = new H1F("hIMProtonPionCut", 250, 1.0, 2.5)
-hIMProtonPionCut.setTitle("Invariant mass of Proton-Pion  with no cuts");
+H1F hIMProtonPionCut = new H1F("hIMProtonPionCut", 250, 1.0, 1.25)
+hIMProtonPionCut.setTitle("Invariant mass of Proton-Pion  with cuts");
 
-TCanvas c1 = new TCanvas("c1", 500, 500);
-TCanvas c2 = new TCanvas("c2", 500, 500);
+TCanvas c1 = new TCanvas("c1", 1000, 1000);
+
+c1.divide(2,2);
+c1.getCanvas().initTimer(1000);
+c1.cd(0).draw(hIMProtonPion);
+c1.cd(1).draw(hMMProtonElectronCut);
+c1.cd(2).draw(hIMProtonPionCut);
+c1.cd(3).draw(hMMProtonElectron);
+
+
+
+/*TCanvas c2 = new TCanvas("c2", 500, 500);
 TCanvas c3 = new TCanvas("c3", 500, 500);
 TCanvas c4 = new TCanvas("c4", 500, 500);
 TCanvas c5 = new TCanvas("c5", 500, 500);
-
+*/
 //String directory = "/w/hallb-scifs17exp/clas12/viducic/data/clas12/lambda/tagger";
 //String directory = "/w/hallb-scifs17exp/clas12/viducic/data/clas12/lambda/forward";
 String directory = "/home/tylerviducic/research/rho/clas12/data/lambda/forward";
@@ -35,7 +45,7 @@ reader.open();
 Event event = new Event();
 Bank particle = new Bank(reader.getSchemaFactory().getSchema("REC::Particle"));
 
-EventFilter filter = new EventFilter("11:2212:-211:321:Xn");
+EventFilter filter = new EventFilter("11:2212:-211:Xn:X+:X-");
 
 while (reader.hasNext()){
     reader.nextEvent(event);
@@ -62,10 +72,4 @@ while (reader.hasNext()){
 
 }
 
-c1.draw(hMMProtonElectronCut);
-c2.draw(hIMProtonPion);
-c3.draw(hMMElectronKaon);
 
-
-c4.draw(hMMProtonElectron);
-c5.draw(hIMProtonPionCut);
