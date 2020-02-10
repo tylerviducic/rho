1bf88901ec2b110ef12f921bdacd859d6a664a73
Tyler Viducic <tyler.viducic@gmail.com>
2020-02-10 14:59:50 -0500

    added missing pion-electron

--- clas12/scripts/pi0Photoproduction.groovy
+++ clas12/scripts/pi0Photoproduction.groovy
@@ -15,6 +15,8 @@ H1F hMissingPEGamGam = new H1F("hMissingPEGamGam", 100, -0.1, 0.1);
 hMissingPEGamGam.setTitle("Missing mass^2 of proton, electron, and first two photons");
 H1F hMissingEnergy = new H1F("missingEnergy", 80, 0, 0.4);
 hMissingEnergy.setTitle("missing momentum of proton, electron, and first two photons");
+H1F hMissingPionElectron = new H1F("hMissingPionElectron", 100, 0.5, 1.5);
+hMissingPionElectron.setTitle("Missing mass of pion (gam1 and gam2) and electron");
 
 TCanvas c1 = new TCanvas("c1", 1000, 1000);
 c1.divide(2, 2);
@@ -22,7 +24,8 @@ c1.getCanvas().initTimer(1000);
 c1.cd(0).draw(hPi0);
 c1.cd(1).draw(hMissingPE);
 c1.cd(2).draw(hMissingPEGamGam);
-c1.cd(3).draw(hMissingEnergy);
+//c1.cd(3).draw(hMissingEnergy);
+c1.cd(3).draw(hMissingPionElectron);
 
 String directory = "/work/clas12/viducic/data/clas12/pion/forward/";
 
@@ -45,13 +48,15 @@ while (reader.hasNext()){
     //Particle pi0 = getBestPi0(physicsEvent);
     Particle missingPE = physicsEvent.getParticle("[b] + [t] - [2212] - [11]");
     Particle missingPEGamGam = physicsEvent.getParticle("[b] + [t] - [2212] - [11] - [22,0] - [22,1]");
+    Particle missingPi0E = physicsEvent.getParticle("[b] + [t] - [11] - [22, 0] - [22, 1]");
 
 
     hMissingPEGamGam.fill(missingPEGamGam.mass2());
     hPi0.fill(pi0.mass());
     hMissingEnergy.fill(missingPEGamGam.p());
-    if(pi0.mass() > 0.1 && pi0.mass() < 0.16 && Math.abs(missingPEGamGam.mass2()) < 0.02 && missingPEGamGam.p() < 0.1){
+    if(pi0.mass() > 0.1 && pi0.mass() < 0.16 && Math.abs(missingPEGamGam.mass2()) < 0.02 && missingPEGamGam.p() < 0.15){
         hMissingPE.fill(missingPE.mass());
+        hMissingPionElectron.fill(missingPi0E.mass());
     }
 }
 
