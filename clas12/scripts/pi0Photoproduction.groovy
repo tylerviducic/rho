8f26cec748edb4a131c87e076637ceb221a69a24
Tyler Viducic <tyler.viducic@gmail.com>
2020-02-11 15:34:05 -0500

    added missing pion-electron

--- clas12/scripts/pi0Photoproduction.groovy
+++ clas12/scripts/pi0Photoproduction.groovy
@@ -56,7 +56,7 @@ while (reader.hasNext()){
     //if(pi0.mass() > 0.1 && pi0.mass() < 0.16 && Math.abs(missingPEGamGam.mass2()) < 0.02){
     if(pi0.mass() > 0.1 && pi0.mass() < 0.16 && missingPi0E.mass() > 0.8 && missingPi0E.mass() < 1.0){
         hMissingPE.fill(missingPE.mass());
-        hMissingPionElectron.fill(missingPi0E.mass());
+        //hMissingPionElectron.fill(missingPi0E.mass());
     }
 }
 
