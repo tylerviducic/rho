1c5b74f6158649426a245d6f67a3d8fdb9c802b3
Tyler Viducic <tyler.viducic@gmail.com>
2020-03-10 11:06:55 -0400

    python + skim run test

--- clas12/scripts/filterRunner.py
+++ clas12/scripts/filterRunner.py
@@ -1,19 +1,25 @@
 import os
-import subprocess
 import glob
 import pickle
 
 pickle_in = open('filteredRuns.p', 'rb')
 filtered_runs = pickle.load(pickle_in)
 pickle_in.close()
-directories = glob.glob("path/to/directory/*")
+directories = glob.glob("/volatile/clas12/rg-a/production/pass0/physTrain/dst/recon/*")
 
-command = 'rg rgaMasterFiler.groovy'
+command = 'rg /w/hallb-scifs17exp/clas12/viducic/rho/clas12/scripts/rgaMasterFiler.groovy'
 
 for directory in directories:
-    if directory not in filtered_runs:
-        cmd_ex = command + directory
+    if directory not in filtered_runs and os.path.isdir(directory):
+        os.system("mv rga_master.hipo rga_skimmed_old.hipo")
+        cmd_ex = command + " " + directory
+        os.system(cmd_ex)
+        combine_command = "/home/viducic/jaw-2.0/bin/hipoutils.sh -merge -o rga_skimmed_master.hipo rga_skimmed.hipo rga_skimmed_old.hipo"
+        os.system(combine_command)
+        os.system('rm rga_skimmed_old.hipo')
         pickle_out = open('filteredRuns.p', 'wb')
         pickle.dump(directory, pickle_out)
         pickle_out.close()
+
+print("done!")
 
--- clas12/scripts/rgaMasterFilter.groovy
+++ clas12/scripts/rgaMasterFilter.groovy
@@ -7,8 +7,8 @@ import org.jlab.jnp.physics.PhysicsEvent
 import org.jlab.jnp.reader.DataManager
 
 
-String directory = "/volatile/clas12/users/clas12/rich/dst/recon/005038/";
-//String directory = args[0];
+//String directory = "/volatile/clas12/users/clas12/rich/dst/recon/005038/";
+String directory = args[0];
 //String file = "/volatile/clas12/users/clas12/rich/dst/recon/005038/rec_clas_005038.evio.00615-00619.hipo"
 //need way to only skim files tht have not been skimmed yet. easy way would be to write a file with run rumbers skimmed
 //if the run number is in the file, move to the next one
