f34823c52f0c076999746a6b63429da1f88d62de
Tyler Viducic <tyler.viducic@gmail.com>
2020-04-05 12:40:07 -0400

    whoops. forgot the electron + typo fix

--- clas12/scripts/pi0FourGamma.groovy
+++ clas12/scripts/pi0FourGamma.groovy
@@ -34,7 +34,7 @@ H1F hmm2 = new H1F("mm2", 100, -0.5, 0.5);
 hmm2.setTitle("Missing mass2 of all particles");
 
 H1F hmxP = new H1F("mxP", 100, 0.5, 1.5);
-hmxP.setTitle("Missing mass of electron and pi+ pi-");
+hmxP.setTitle("Missing mass of electron and pi0pi0");
 
 TCanvas c1 = new TCanvas("c1", 1000, 1000);
 c1.divide(2, 2);
@@ -102,10 +102,10 @@ while (reader.hasNext()) {
 
         //Particle f0 = physicsEvent.getParticle("[22, 0] + [22, 1] + [22, 2] + [22, 3]");
         Particle missingePPi0Pi0 = physicsEvent.getParticle("[b] + [t] - [2212] - [11] - [22,0] - [22,1] - [22,2] - [22,3]");
-        Particle missingePi0Pi0 = physicsEvent.getParticle("[b] + [t]");
+        Particle missingePi0Pi0 = physicsEvent.getParticle("[b] + [t] - [11]");
 
          //double missingP = Math.sqrt(missingePPi0Pi0.px()/missingePPi0Pi0.p() * missingePPi0Pi0.px()/missingePPi0Pi0.p() + missingePPi0Pi0.py()/missingePPi0Pi0.p() * missingePPi0Pi0.py()/missingePPi0Pi0.p())
-
+        hmm2.fill(missingePPi0Pi0.mass2());
         hmp.fill(missingePPi0Pi0.px()/missingePPi0Pi0.p(), missingePPi0Pi0.py()/missingePPi0Pi0.p());
 
         if (sector0 == -1 || sector1 == -1 || sector2 == -1 || sector3 == -1
@@ -152,7 +152,6 @@ while (reader.hasNext()) {
             missingePi0Pi0.combine(testPion2, -1);
 
             hpionpion.fill(pion1.mass(), pion2.mass());
-            hmm2.fill(missingePPi0Pi0.mass2());
             hmxP.fill(missingePi0Pi0.mass());
             if (pion1.mass() > 0.12 && pion1.mass() < 0.15 && pion2.mass() > 0.12 && pion2.mass() < 0.15) {
                 hf0.fill(f0.mass());
