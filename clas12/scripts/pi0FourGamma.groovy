ae0fb153ecebb80f4a503696f84f447569405135
Tyler Viducic <tyler.viducic@gmail.com>
2020-04-09 08:53:21 -0400

    relaxing cut to 0.4 photon momentum

--- clas12/scripts/pi0FourGamma.groovy
+++ clas12/scripts/pi0FourGamma.groovy
@@ -71,7 +71,7 @@ Bank calorimeter = new Bank(reader.getSchemaFactory().getSchema("REC::Calorimete
 
 EventFilter eventFilter = new EventFilter("11:2212:22:22:22:22:Xn");
 
-double gamCut = 0.5;
+double gamCut = 0.3;
 
 while (reader.hasNext()) {
     reader.nextEvent(event);
