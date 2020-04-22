698f3a9c2061e59d4d4b524533fd87dc64bd45ce
Tyler Viducic <tyler.viducic@gmail.com>
2020-04-22 15:20:05 -0400

    pion photoproduction filter added

--- clas12/scripts/pi0PhotoproductionFilter.groovy
+++ clas12/scripts/pi0PhotoproductionFilter.groovy
@@ -13,44 +13,34 @@ import org.jlab.groot.ui.TCanvas
 import org.jlab.jnp.utils.file.FileUtils
 
 
-String directory = "/volatile/clas12/users/clas12/release/6b.5.1/calib/recon/005038/";
-
+//String directory = "/volatile/clas12/users/clas12/release/6b.5.1/calib/recon/005038/";
+String directory = "/cache/clas12/rg-a/production/recon/fall2018/torus-1/pass1/v0/dst/train/skim4";
 
 HipoChain reader = new HipoChain();
 reader.addDir(directory);
 reader.open();
 
-//HipoWriter writer = new HipoWriter(reader.getSchemaFactory());
-//writer.open("/w/hallb-scifs17exp/clas12/viducic/data/clas12/pion/script_filter.hipo");
+HipoWriter writer = new HipoWriter(reader.getSchemaFactory());
+writer.open("/w/hallb-scifs17exp/clas12/viducic/data/clas12/pion/pi0Photoproduction_skim4.hipo");
 
-long eventCounter = 0;
 
 Event event = new Event();
 Bank particle = new Bank(reader.getSchemaFactory().getSchema("REC::Particle"));
 
-EventFilter filter = new EventFilter("11:2212:Xn");
+EventFilter filter = new EventFilter("11:22:22:Xn:X+:X-");
 
 while (reader.hasNext()){
     reader.nextEvent(event);
     event.read(particle);
-    eventCounter++;
-
-    if(eventCounter % 100000 == 0){
-        System.out.println("Event counter = " + eventCounter);
-    }
 
     PhysicsEvent physicsEvent = DataManager.getPhysicsEvent(10.6, particle);
 
-    if(filter.isValid(physicsEvent) && physicsEvent.getParticle(0).pid() == 11
-            && physicsEvent.countByPid(22) >= 2
-            && physicsEvent.getParticleByPid(2212, 0).theta() < Math.toRadians(35)){
-
-        //writer.addEvent(event);
-        System.out.println(physicsEvent.countByPid(22));
+    if(filter.isValid(physicsEvent)){
+        writer.addEvent(event);
     }
 }
 
-//writer.close();
+writer.close();
 
 println("done");
 
