a12db5b077580ebe847c060ce776f11f1b2e737f
Tyler Viducic <tyler.viducic@gmail.com>
2020-05-11 17:04:42 -0400

    photonproduction of pion

--- clas12/scripts/pi0Photoproduction.groovy
+++ clas12/scripts/pi0Photoproduction.groovy
@@ -1,4 +1,5 @@
 import org.jlab.groot.data.H1F
+import org.jlab.groot.data.H2F
 import org.jlab.groot.ui.TCanvas
 import org.jlab.jnp.hipo4.data.Bank
 import org.jlab.jnp.hipo4.data.Event
@@ -7,28 +8,37 @@ import org.jlab.jnp.physics.Particle
 import org.jlab.jnp.physics.PhysicsEvent
 import org.jlab.jnp.reader.DataManager
 
-H1F hPi0 = new H1F("pi0", 50, 0, 0.25);
-hPi0.setTitle("Invariant mass gamma gamma");
-H1F hMissingPE = new H1F("missingPE", 100, 0, 0.25);
-hMissingPE.setTitle("missing mass of proton-electron");
-H1F hMissingPEGamGam = new H1F("hMissingPEGamGam", 100, -0.1, 0.1);
-hMissingPEGamGam.setTitle("Missing mass^2 of proton, electron, and first two photons");
-H1F hMissingEnergy = new H1F("missingEnergy", 80, 0, 0.4);
-hMissingEnergy.setTitle("missing momentum of proton, electron, and first two photons");
-H1F hMissingPionElectron = new H1F("hMissingPionElectron", 100, 0.5, 1.5);
-hMissingPionElectron.setTitle("Missing mass of pion (gam1 and gam2) and electron");
+// ------------------------------------------  Histograms ------------------------------------------------
+H1F hMissingMassEPi0= new H1F("MissingMassEPi0", 100, 0, 2);
+hMissingMassEPi0.setTitle("Missing mass of e'#pi^0");
+
+H1F hMissingMassEPPi0 = new H1F("MissingMassEPi0", 100, -0.5, 0.5);
+hMissingMassEPPi0.setTitle("MM(e'p'#pi^0")
+
+H1F hIMGamGam = new H1F("IMGamGam", 100, 0, 0.3);
+hIMGamGam.setTitle("Invariant mass of #gamma#gamma");
+
+H2F hGamGamPvsTheta = new H2F("GamGamPvsTheta", 200, 0, 6, 35, 0, 35);
+hGamGamPvsTheta.setTitle("Momentum of #gamma#gamma vs opening angle between them");
+hGamGamPvsTheta.setTitleX("P(#gamma#gamma)");
+hGamGamPvsTheta.setTitleY("#theta(#gamma#gamma)");
+
+H1F hQ2 = new H1F("q2", 100, 0, 3);
+hQ2.setTitle("Q^2 of e'");
+
+// ------------------------------------------  Histograms ------------------------------------------------
 
 TCanvas c1 = new TCanvas("c1", 1000, 1000);
-c1.divide(2, 2);
+c1.divide(3, 2);
 c1.getCanvas().initTimer(1000);
-c1.cd(0).draw(hPi0);
-c1.cd(1).draw(hMissingPE);
-c1.cd(2).draw(hMissingPEGamGam);
-//c1.cd(3).draw(hMissingEnergy);
-c1.cd(3).draw(hMissingPionElectron);
+c1.cd(0).draw(hIMGamGam);
+c1.cd(1).draw(hMissingMassEPi0);
+c1.cd(2).draw(hMissingMassEPPi0);
+c1.cd(3).draw(hGamGamPvsTheta);
+c1.cd(4).draw(hQ2);
 
-String directory = "/work/clas12/viducic/data/clas12/pion/forward/";
-//String directory = "/media/tylerviducic/Elements/clas12/pion/forward";
+
+String directory = "/w/hallb-scifs17exp/clas12/viducic/data/clas12/pion/pi0Photoproduction_skim3.hipo";
 
 HipoChain reader = new HipoChain();
 reader.addDir(directory);
@@ -42,47 +52,77 @@ while (reader.hasNext()){
     event.read(particle);
 
     PhysicsEvent physicsEvent = DataManager.getPhysicsEvent(10.6, particle);
-    //Particle pi0 = physicsEvent.getParticle("[22,0] + [22,1]");
-    Particle pi0 = getBestPi0(physicsEvent);
-    Particle missingPE = physicsEvent.getParticle("[b] + [t] - [2212] - [11, 0]");
-    Particle missingPEGamGam = Particle.copyFrom(physicsEvent.getParticle("[b] + [t] - [2212] - [11, 0]"));
-    missingPEGamGam.combine(Particle.copyFrom(pi0), -1);
-    Particle missingPi0E = Particle.copyFrom(physicsEvent.getParticle("[b] + [t] - [11, 0]"));
-    missingPi0E.combine(Particle.copyFrom(pi0), -1);
 
+    Particle electron = getTaggerElectron(physicsEvent);
 
-    hMissingPEGamGam.fill(missingPEGamGam.mass2());
-    hPi0.fill(pi0.mass());
-    hMissingEnergy.fill(missingPEGamGam.p());
-    if(pi0.mass() > 0.1 && pi0.mass() < 0.16 && Math.abs(missingPEGamGam.mass2()) < 0.02 && missingPEGamGam.p() < 0.2){
-    //if(pi0.mass() > 0.1 && pi0.mass() < 0.16 && missingPi0E.mass() > 0.8 && missingPi0E.mass() < 1.0){
-      //      && Math.abs(missingPEGamGam.mass2()) < 0.02){
-        hMissingPE.fill(missingPE.mass());
-        hMissingPionElectron.fill(missingPi0E.mass());
+    ArrayList<Integer> photonIndexes = getBestPhotons(physicsEvent);
+    Particle photon1 = physicsEvent.getParticle(photonIndexes.get(0));
+    Particle photon2 = physicsEvent.getParticle(photonIndexes.get(1));
+
+    Particle pion = Particle.copyFrom(photon1);
+    pion.combine(Particle.copyFrom(photon2), 1);
+
+    Particle missingEPPi0 = physicsEvent.getParticle("[b] + [t] - [2212] - [11]");
+    Particle missingEPi0 = physicsEvent.getParticle("[b] + [t] - [11]");
+    missingEPi0.combine(Particle.copyFrom(pion), -1);
+    missingEPPi0.combine(Particle.copyFrom(pion), -1);
+
+    double q2 = getQ2(physicsEvent.beamParticle(), electron);
+    double photonTheta = Math.toDegrees(Math.acos(photon1.cosTheta(photon2)));
+
+    hMissingMassEPPi0.fill(missingEPPi0.mass2());
+
+    if (missingEPPi0.mass2() < 0.1){
+        hQ2.fill(q2);
+        hMissingMassEPi0.fill(missingEPi0.mass());
+
+        hGamGamPvsTheta.fill(pion.p(), photonTheta);
+        hIMGamGam.fill(pion.mass());
     }
+
 }
 
 
 System.out.println("done");
 
 
-/////// Methods /////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////    METHODS   //////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-static Particle getBestPi0(PhysicsEvent myPhysicsEvent){
-    int photonCount = myPhysicsEvent.countByPid(22);
-    for(int i = 0; i < photonCount-1; i++){
-        for(int j = i+1; j<photonCount; j++){
-            Particle pi0 = Particle.copyFrom(myPhysicsEvent.getParticleByPid(22, i));
-            Particle gam2 = Particle.copyFrom(myPhysicsEvent.getParticleByPid(22, j));
+public static ArrayList<Integer> getBestPhotons(PhysicsEvent physicsEvent){
+    ArrayList<Integer> photons = new ArrayList<>();
+    int numPhotons = physicsEvent.countByPid(22);
 
-            pi0.combine(gam2, 1);
-            if(pi0.mass() < 0.16 && pi0.mass() > 0.1){
-                return pi0
+    for(int i = 0; i < numPhotons - 1; i++){
+        Particle photon1 = Particle.copyFrom(physicsEvent.getParticleByPid(22, i));
+        for (int j = i + 1; j < numPhotons; j++){
+            Particle photon2 = Particle.copyFrom(physicsEvent.getParticleByPid(22, j));
+            Particle pi0 = Particle.copyFrom(photon1);
+            pi0.combine(photon2, 1);
+
+            if (pi0.mass() > 0.12 && pi0.mass() < 0.15){
+                photons.add(physicsEvent.getParticleIndex(22, i));
+                photons.add(physicsEvent.getParticleIndex(22, j));
+                return photons;
             }
         }
     }
-    Particle pi0 = Particle.copyFrom(myPhysicsEvent.getParticleByPid(22, 0));
-    Particle gam2 = Particle.copyFrom(myPhysicsEvent.getParticleByPid(22, 1));
-    pi0.combine(gam2, 1);
-    return pi0;
+    photons.add(physicsEvent.getParticleIndex(22, 0));
+    photons.add(physicsEvent.getParticleIndex(22, 1));
+    return photons;
+}
+
+public static Particle getTaggerElectron(PhysicsEvent physicsEvent){
+    for(int i = 0; i < physicsEvent.countByPid(11); i++){
+        Particle electron = physicsEvent.getParticleByPid(22, i);
+        if (electron.theta() < Math.toRadians(4)){
+            return electron;
+        }
+    }
+    return physicsEvent.getParticleByPid(22, 0);
+}
+
+public static double getQ2(Particle particle1, Particle particle2){
+    return 4 * particle1.e() * particle2.e() * Math.sin(particle2.theta() /2) * Math.sin(particle2.theta()/2);
 }
