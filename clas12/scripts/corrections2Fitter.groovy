0502e0c7d12e139813919b58c306fc7184d409d5
Tyler Viducic <tyler.viducic@gmail.com>
2020-06-23 11:28:16 -0400

    bug fixes

--- clas12/scripts/corrections2Fitter.groovy
+++ clas12/scripts/corrections2Fitter.groovy
@@ -38,7 +38,7 @@ dir2.readFile("/w/hallb-scifs17exp/clas12/viducic/rho/clas12/results/energyCorre
 for(int i = 0; i < 10; i++){
     Double energy = 4.5 + i * 0.5;
     String energyString = energy.toString();
-    H1F histo = (H1F) dir1.getObject("/PionsBinned/e(gam)=" + energyString);
+    H1F histo = (H1F) dir2.getObject("/PionsBinned/e(gam)=" + energyString);
 
     StatNumber statNumber = getMassDataPoint(histo);
 
@@ -60,9 +60,9 @@ public static StatNumber getMass2DataPoint(H1F histo){
 
     DataFitter.fit(func, histo, "");
 
-//    TCanvas c1 = new TCanvas("c1", 500, 500);
-//    c1.draw(histo);
-//    c1.draw(func, "same");
+    TCanvas c1 = new TCanvas("c1", 500, 500);
+    c1.draw(histo);
+    c1.draw(func, "same");
 
     StatNumber statNumber = new StatNumber(func.parameter(1).value(), func.parameter(1).error());
     statNumber.divide(new StatNumber(0.135 * 0.135, 0.0000005 * 0.0000005));
@@ -71,20 +71,19 @@ public static StatNumber getMass2DataPoint(H1F histo){
 }
 
 public static StatNumber getMassDataPoint(H1F histo){
-//    F1D func = new F1D("f1", "[amp]*gaus(x,[mean],[sigme])+[p0]+[p1]*x+[p2]*x*x", 0.01, 0.03);
-//    func.setParameter(0, 100);
-//    func.setParameter(1, 0.135);
-//    func.setParameter(2, 0.01);
-//
-//    DataFitter.fit(func, histo, "");
-//
-//    StatNumber statNumber = new StatNumber(func.parameter(1).value(), func.parameter(1).error());
-//    statNumber.divide(new StatNumber(0.135, 0.0000005));
+    F1D func = new F1D("f1", "[amp]*gaus(x,[mean],[sigme])+[p0]+[p1]*x+[p2]*x*x", 0.01, 0.03);
+    func.setParameter(0, 100);
+    func.setParameter(1, 0.135);
+    func.setParameter(2, 0.01);
+
+    DataFitter.fit(func, histo, "");
+
+    StatNumber statNumber = new StatNumber(func.parameter(1).value(), func.parameter(1).error());
+    statNumber.divide(new StatNumber(0.135, 0.0000005));
 
     TCanvas c1 = new TCanvas("c1", 500, 500);
     c1.draw(histo);
-//    c1.draw(func, "same");
+    c1.draw(func, "same");
 
-    return new StatNumber(0, 0);
-//    return statNumber;
+    return statNumber;
 }
