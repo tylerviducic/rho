21a41c250ef875058a2f30acfeff8f048bc20c47
Tyler Viducic <tyler.viducic@gmail.com>
2020-06-25 10:16:35 -0400

    fitting the protons

--- clas12/scripts/momentumCorrectionFitter.groovy
+++ clas12/scripts/momentumCorrectionFitter.groovy
@@ -15,28 +15,6 @@ protonDir.readFile("/w/hallb-scifs17exp/clas12/viducic/rho/clas12/results/energy
 //ArrayList<Double> mass = new ArrayList<>();
 //ArrayList<Double> error = new ArrayList<>();
 
-TDirectory dir = new TDirectory();
-dir.mkdir("/Plot");
-
-//mass.add(1.228);
-//mass.add(1.052);
-//mass.add(1.025);
-//mass.add(1.013);
-//mass.add(1.02);
-//mass.add(1.011);
-//mass.add(1.003);
-//mass.add(0.948);
-//
-//
-//error.add(5.35E-3);
-//error.add(1.696E-3);
-//error.add(1.088E-3);
-//error.add(7.068E-4);
-//error.add(8.107E-4);
-//error.add(6.47E-4);
-//error.add(4.779E-4);
-//error.add(8.2E-4);
-
 TCanvas c2 = new TCanvas("c2", 1000, 1000);
 c2.divide(2, 4);
 
@@ -59,26 +37,23 @@ for(int i = 0; i < 8; i++){
     graph.addPoint(4.5 + (i * 0.5), dataPoint.number(), 0, dataPoint.error());
 }
 
-dir.cd("/Plot");
-dir.addDataSet(graph);
-dir.writeFile("/w/hallb-scifs17exp/clas12/viducic/rho/clas12/results/missingProtonResult.hipo");
+//TDirectory dir = new TDirectory();
+//dir.mkdir("/Plot");
+//dir.cd("/Plot");
+//dir.addDataSet(graph);
+//dir.writeFile("/w/hallb-scifs17exp/clas12/viducic/rho/clas12/results/missingProtonResult.hipo");
 
 TCanvas c1 = new TCanvas("c1", 1000, 1000);
 System.out.println("printing graph");
 c1.draw(graph);
 
-//F1D func = new F1D("func", "[p0] + [p1]/x + [p2]/(x*x) + [p3]/(x*x*x)", 4.5, 8.0);
-//F1D funcW = new F1D("funcW", "[p0] + [p1]/x + [p2]/(x*x) + [p3]/(x*x*x)", 4.5, 8.0);
+
 F1D func3 = new F1D("func3", "[p0]+[p1]*x+[p2]*x*x+[p3]*x*x*x", 4.5, 8.0);
 
-//func.setLineColor(2);
-//funcW.setLineColor(4);
 func3.setLineColor(3);
 
-//DataFitter.fit(func, graph, "N");
-//DataFitter.fit(funcW, graph, "W");
 DataFitter.fit(func3, graph, "");
 
-//c1.draw(func, "same");
-//c1.draw(funcW, "same");
 c1.draw(func3, "same");
+
+func3.show();
