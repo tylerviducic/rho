81134cce810ef60c7b400f888238dbf85b63a635
Tyler Viducic <tyler.viducic@gmail.com>
2020-06-30 12:36:33 -0400

    trying pol-1(2) fit

--- clas12/scripts/energyCorrectionFitter.groovy
+++ clas12/scripts/energyCorrectionFitter.groovy
@@ -1,82 +1,57 @@
 import org.jlab.groot.data.GraphErrors
+import org.jlab.groot.data.TDirectory
 import org.jlab.groot.fitter.DataFitter
 import org.jlab.groot.math.F1D
 import org.jlab.groot.math.StatNumber
 import org.jlab.groot.ui.TCanvas
+import org.jlab.groot.data.H1F
+
+TDirectory dir1 = new TDirectory();
+dir1.readFile("/w/hallb-scifs17exp/clas12/viducic/rho/clas12/results/energyCorrections.hipo");
 
 GraphErrors graph = new GraphErrors("MassRatioVsEnergy");
 graph.setTitle("Pion Mass Ratio vs Photon Energy");
 
-ArrayList<Double> mass = new ArrayList<>();
-ArrayList<Double> error = new ArrayList<>();
+//ArrayList<Double> mass = new ArrayList<>();
+//ArrayList<Double> error = new ArrayList<>();
+//
+//mass.add(0.13425468758675885);
+//mass.add(0.1363027099818894);
+//mass.add(0.13654244932898696);
+//mass.add(0.13691479190231992);
+//mass.add(0.13735092286542408);
+//mass.add(0.13686670992767505);
+//mass.add(0.13759914635520898);
+//mass.add(0.13843131323153757);
+//mass.add(0.13813582623651843);
+//
+//error.add(0.0003392);
+//error.add(0.0002641);
+//error.add(0.0002759);
+//error.add(0.0002701);
+//error.add(0.0002572);
+//error.add(0.0003295);
+//error.add(0.0003959);
+//error.add(0.0003564);
+//error.add(0.0004967);
 
-mass.add(0.13425468758675885);
-mass.add(0.1363027099818894);
-mass.add(0.13654244932898696);
-mass.add(0.13691479190231992);
-mass.add(0.13735092286542408);
-mass.add(0.13686670992767505);
-mass.add(0.13759914635520898);
-mass.add(0.13843131323153757);
-mass.add(0.13813582623651843);
+for (int i = 0; i < 10; i++){
+    Double energy = 1.0 + i * 0.17;
+    H1F histo = (H1F) dir1.getObject("/PionsBinned/e(gam)=" + energy.toString());
 
-error.add(0.0003392);
-error.add(0.0002641);
-error.add(0.0002759);
-error.add(0.0002701);
-error.add(0.0002572);
-error.add(0.0003295);
-error.add(0.0003959);
-error.add(0.0003564);
-error.add(0.0004967);
+    F1D func = new F1D("f"+i, "[amp]*gaus(x,[mean],[sigme])+[p0]+[p1]*x+[p2]*x*x", 0.1, 0.2);
+    DataFitter.fit(func, histo, "");
+    StatNumber statNumber = new StatNumber(func.parameter(1).value(), func.parameter(1).error());
+    statNumber.divide(new StatNumber(0.135, 0.0000005));
 
-for(int i = 0; i < mass.size(); i++){
-    StatNumber dataPoint = new StatNumber(mass.get(i), error.get(i));
-    dataPoint.divide(new StatNumber(0.135, 0.0000005));
-    graph.addPoint(1 + (i * 0.17), dataPoint.number(), 0, dataPoint.error());
+    graph.addPoint(energy, statNumber.number(), 0, statNumber.error());
 }
 
+F1D func2 = new F1D("correctionFunction", "[p0]+[p1]/x+[p2]/(x*x)", 0.99, 3);
+DataFitter.fit(func2, graph, "");
+
+
 TCanvas c1 = new TCanvas("c1", 1000, 1000);
-System.out.println("printing graph");
 c1.draw(graph);
+c1.draw(func2, "same");
 
-F1D func = new F1D("func", "[p0] + [p1]/x + [p2]/(x*x) + [p3]/(x*x*x)", 0.99, 2.5);
-F1D funcW = new F1D("funcW", "[p0] + [p1]/x + [p2]/(x*x) + [p3]/(x*x*x)", 0.99, 2.5);
-func.setLineColor(2);
-funcW.setLineColor(4);
-
-DataFitter.fit(func, graph, "");
-DataFitter.fit(funcW, graph, "W");
-
-c1.draw(func, "same");
-c1.draw(funcW, "same");
-
-//Fit for E(#gamma) = 1.00
-//Mean mass = 0.13425468758675885 --- Error = 3.392943496316817E-4
-//[fit-benchmark] Time = 0.019 , Iterrations = 477
-//Fit for E(#gamma) = 1.17
-//Mean mass = 0.1363027099818894 --- Error = 2.641006240861557E-4
-//[fit-benchmark] Time = 0.011 , Iterrations = 492
-//Fit for E(#gamma) = 1.34
-//Mean mass = 0.13654244932898696 --- Error = 2.759337080867641E-4
-//[fit-benchmark] Time = 0.011 , Iterrations = 500
-//Fit for E(#gamma) = 1.51
-//Mean mass = 0.13691479190231992 --- Error = 2.7009250848417815E-4
-//[fit-benchmark] Time = 0.011 , Iterrations = 497
-//Fit for E(#gamma) = 1.68
-//Mean mass = 0.13735092286542408 --- Error = 2.572143850395676E-4
-//[fit-benchmark] Time = 0.009 , Iterrations = 499
-//Fit for E(#gamma) = 1.85
-//Mean mass = 0.13686670992767505 --- Error = 3.2948972804102864E-4
-//[fit-benchmark] Time = 0.011 , Iterrations = 546
-//Fit for E(#gamma) = 2.02
-//Mean mass = 0.13759914635520898 --- Error = 3.9588158298374597E-4
-//[fit-benchmark] Time = 0.060 , Iterrations = 588
-//Fit for E(#gamma) = 2.19
-//Mean mass = 0.13843131323153757 --- Error = 3.563828837928601E-4
-//[fit-benchmark] Time = 0.009 , Iterrations = 714
-//Fit for E(#gamma) = 2.36
-//Mean mass = 0.13813582623651843 --- Error = 4.966868550319091E-4
-//[fit-benchmark] Time = 0.008 , Iterrations = 745
-//Fit for E(#gamma) = 2.53
-//Mean mass = 0.14167637098214056 --- Error = 4.047111634769342E-4
